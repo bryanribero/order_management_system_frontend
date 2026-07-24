04d84b33590680cdb72a9d9e25000819826ca0bc
Bryan <bryanribero@hotmail.com>
2026-07-24 20:38:44 -0300

    chore: Creacion de la estructura para usar rutas

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <button onClick={() => setCount(count + 1)}>Contador</button>
-        {count}
-      </div>
+      <Routes></Routes>
     </>
   )
 }
